0109ec5be0c70bc72ee149a53e0b248022d00345
Shivam Shukla <ss1363422@gmail.com>
2021-01-18 17:57:22 +0530

    (Second commit)

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const express=require('express');
-const app=express();
-console.log('jkcbsc');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.post('/login', function (req, res) {
+    console.log("request", req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+    const e = await userModel.findOne({ email: email });
+    console.log(e);
+    if (e) {
+        if (e.password === password) {
+            res.send("Login successful");
+        }
+        else {
+            res.send("Email or Password is incorrect");
+        }
+    }
+});
+app.listen(3000);
+console.log('Server is listening at port 3000');
